6a3bbd9bfcd7241a9391911998b6b74a46334c9a
kwakseobang <iii6602@naver.com>
2024-06-30 16:30:33 +0900

    first commit

---
+++ AppleClock/AppleClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleClockApp.swift
+//  AppleClock
+//
+//  Created by 곽서방 on 6/30/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
